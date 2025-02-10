1347a494f8500daa822978344f014ac67103ab75
pimentelmota <pimentelmota@google.com>
2025-02-10 17:22:55 +0000

    Update setup_vm.sh
    Now search for the latest version

--- setup_vm.sh
+++ setup_vm.sh
@@ -3,7 +3,7 @@
 DEFAULT_BURP_VERSION="2024.12.1"  # Default version if fetching fails
 
 # Use curl to fetch the webpage and grep to find the version number
-BURP_VERSION_RAW=$(curl -s "${BURP_VERSION_PAGE_URL}" | grep -oP 'Burp Suite Community Edition \K\d+\.\d+\.\d+')
+BURP_VERSION_RAW=$(curl -s "https://portswigger.net/burp/releases" | grep -oP 'Professional / Community \K\d+\.\d+\.\d+' | head -n 1)
 
 # Check if version extraction was successful
 if [ -z "${BURP_VERSION_RAW}" ]; then
