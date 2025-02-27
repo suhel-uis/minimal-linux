2ebde8c37ccafc7fe2450cb37adfd371e1e53232
pimentelmota <pimentelmota@google.com>
2025-02-27 09:37:28 +0000

    Update setup_display.sh
    adding add the remote desktop configuration

--- setup_display.sh
+++ setup_display.sh
@@ -9,23 +9,83 @@ CHROME_REMOTE_DESKTOP_CODE="$2"
 PRE_CONFIGURED_PIN="123456"
 shift
 
+DEFAULT_BURP_VERSION="2025.1.1"
+
+# Fetch Burp version (improved error handling)
+BURP_VERSION_RAW=$(curl -s "https://portswigger.net/burp/releases" | grep -oP 'Professional / Community \K\d+\.\d+\.\d+' | head -n 1)
+
+if [ -z "${BURP_VERSION_RAW}" ]; then
+  echo "Warning: Could not automatically determine the latest Burp Suite version."
+  echo "Falling back to default Burp Suite version: ${DEFAULT_BURP_VERSION}"
+  BURP_VERSION="${DEFAULT_BURP_VERSION}"
+else
+  BURP_VERSION="${BURP_VERSION_RAW}"
+  echo "Latest Burp Suite Community Edition version found: ${BURP_VERSION}"
+fi
+
+# Update the packages lists and install apt-fast
+echo "Installing apt-fast..."
+sudo add-apt-repository ppa:apt-fast/stable -y
+sudo apt update -yqq
+echo debconf apt-fast/maxdownloads string 16 | sudo debconf-set-selections
+echo debconf apt-fast/dlflag boolean true | sudo debconf-set-selections
+echo debconf apt-fast/aptmanager string apt-get | sudo debconf-set-selections
+sudo apt install apt-fast -yqq
+
+# Check again if apt-fast is installed after attempting installation
+if command -v apt-fast &> /dev/null; then
+  APT_INSTALL_CMD="apt-fast"
+  echo "apt-fast installed successfully. Using apt-fast for package installations."
+ else
+  APT_INSTALL_CMD="apt"
+  echo "apt-fast installation failed. Falling back to using apt for package installations."
+fi
+
 # Download all files upfront in parallel - Chrome Remote Desktop, Google Chrome Stable, VS Code, Burp Suite Community Edition.
 echo "Downloading installation files in parallel..."
-wget -q "https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb" -O chrome-remote-desktop_current_amd64.deb 
+wget -q "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O google-chrome-stable_current_amd64.deb &
+wget -q "https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb" -O chrome-remote-desktop_current_amd64.deb &
+wget -q "https://portswigger.net/burp/releases/startdownload?product=community&version=${BURP_VERSION}&type=Linux" -O burpsuite &
+
+# Install packages Gui
+echo "Installing minimal desktop environment and applications..."
+sudo ${APT_INSTALL_CMD} install -yqq ubuntu-desktop-minimal --no-install-recommends network-manager file-roller
+GUI_INSTALL_PID=$! # Capture the process ID of the GUI installation
 
 wait # Wait for all background wget processes to complete
 echo "Downloads completed."
 
+wait $GUI_INSTALL_PID # Wait for the GUI installation to complete
+echo "GUI installation completed."
+
+# Install Google Chrome Stable
+echo "Installing Google Chrome Stable..."
+sudo ${APT_INSTALL_CMD} install -yqq "./google-chrome-stable_current_amd64.deb"
+rm "./google-chrome-stable_current_amd64.deb"
+
 # Install Chrome Remote Desktop
 echo "Installing Chrome Remote Desktop..."
-sudo apt install -yqq "./chrome-remote-desktop_current_amd64.deb"
+sudo ${APT_INSTALL_CMD} install -yqq "./chrome-remote-desktop_current_amd64.deb"
 rm "./chrome-remote-desktop_current_amd64.deb"
 
+# Install Burp Suite Community Edition
+echo "Installing Burp Suite Community Edition (Version: ${BURP_VERSION})..."
+sudo chmod +x burpsuite
+sudo ./burpsuite -q
+rm burpsuite
+
+# Install VsCode
+echo "Installing VsCode..."
+sudo snap install --classic code
+wait # Wait for the VsCode installation to complete
+echo "VsCode installation completed."
+
 # Start Chrome Remote Desktop host if code is provided
-if [ -n "${CHROME_REMOTE_DESKTOP_CODE}" ]; then
+if [ -n "${CHROME_ROMETE_USER_NAME}" -a -n "${CHROME_REMOTE_DESKTOP_CODE}" ]; then
   echo "Starting Chrome Remote Desktop..."
   # Run start-host as the current user, not as root directly
      DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="${CHROME_REMOTE_DESKTOP_CODE}" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --user-name="${CHROME_ROMETE_USER_NAME}" --pin="${PRE_CONFIGURED_PIN}"
+  echo "Finish Starting Chrome Remote Desktop"
   else
   echo "Chrome Remote Desktop start skipped because code was not provided."
 fi
